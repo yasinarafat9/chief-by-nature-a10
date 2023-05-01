1e88c5ed386c5c5f3145e7cf722c25e391d27ee0
yasinarafat9 <yasinarafat9612@gmail.com>
2023-05-01 23:40:06 +0600

    Installed react router, bootstrap and firebase

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCbwNZav0vZUB54gE99AtewnHp7-WqgOlU",
+  authDomain: "chef-by-nature-client.firebaseapp.com",
+  projectId: "chef-by-nature-client",
+  storageBucket: "chef-by-nature-client.appspot.com",
+  messagingSenderId: "318930677091",
+  appId: "1:318930677091:web:f4f971f0c8755b0cd0f756"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;

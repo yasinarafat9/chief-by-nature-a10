5a5cf21fb02851f7d6116107e6bdae0a1ff57720
yasinarafat9 <yasinarafat9612@gmail.com>
2023-05-02 15:42:21 +0600

    footer section

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
 import './Footer.css'
+import logos from '../../assets/images/logos .png'
 
 const Footer = () => {
     return (
         <div>
-            <h2>This is Footer</h2>
+            <footer>
+                <h2>This is Footer</h2>
+                <div className=' footer-bg '>
+                    <br />
+                    <h2>Chef By Nature</h2>
+                    <div className='d-flex mx-auto p-3 logos-bg'>
+                        <img src={logos} alt="" />
+                        <img src={logos} alt="" />
+                        <img src={logos} alt="" />
+                    </div>
+                    <br />
+                    <div className=' mx-5 px-5'>
+
+                        <br />
+                        <p>Each recipe page includes a clear and concise list of ingredients, step-by-step instructions, and a photo of the finished dish. Users can also leave comments and ratings on each recipe, allowing others to see how successful they were in replicating the dish.</p>
+                    </div>
+                    <br />
+
+                    <p>Copywrite of this website</p>
+                    <br />
+                    <br />
+                </div>
+            </footer>
         </div>
     );
 };
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from '../components/Header/Header';
+import Footer from '../components/Footer/Footer';
 
 const Main = () => {
     return (
         <div>
             <Header></Header>
             <Outlet></Outlet>
-            <footer></footer>
+            <Footer></Footer>
         </div>
     );
 };

14ac6fa424341ddc8f2695202bc65d5eefa990a9
yasinarafat9 <yasinarafat9612@gmail.com>
2023-05-02 21:55:27 +0600

    installed react icons and applied on the footer as social media icons

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Footer.css'
 import logos from '../../assets/images/logos .png'
+import { FaYoutube, FaFacebook, FaLinkedin, FaTwitterSquare } from "react-icons/fa";
 
 const Footer = () => {
     return (
@@ -10,6 +11,7 @@ const Footer = () => {
                 <div className=' footer-bg '>
                     <br />
                     <h2>Chef By Nature</h2>
+                    <br />
                     <div className='d-flex mx-auto p-3 logos-bg'>
                         <img src={logos} alt="" />
                         <img src={logos} alt="" />
@@ -17,7 +19,14 @@ const Footer = () => {
                     </div>
                     <br />
                     <div className=' mx-5 px-5'>
-
+                    <div>
+                        {/* <h6>Find and follow us on</h6> */}
+                        <FaTwitterSquare className='social-icons'/>
+                        <FaYoutube className='social-icons'/>
+                        <FaFacebook className='social-icons'/>
+                        <FaLinkedin className='social-icons'/>
+                    </div>
+                    
                         <br />
                         <p>Each recipe page includes a clear and concise list of ingredients, step-by-step instructions, and a photo of the finished dish. Users can also leave comments and ratings on each recipe, allowing others to see how successful they were in replicating the dish.</p>
                     </div>

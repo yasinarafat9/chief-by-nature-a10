daf96bc0f4bd2176d76d483f1b33d822245594ae
yasinarafat9 <yasinarafat9612@gmail.com>
2023-05-02 22:35:13 +0600

    blog page question and answer complete

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
 import './Blog.css'
+// import chef61 from '../../assets/images/chef 6 (1).jpg'
 
 const Blog = () => {
     return (
-        <div>
+        <div className='total-blog-container'>
+            {/* <img src={chef61} alt="" /> */}
             <h2>Blog section</h2>
+            <div className='blog-container'>
+
+                <h2>Question and Answers</h2>
+                <div className='blog-all-texts'>
+                    <h4>Question 1: Tell us the differences between uncontrolled and controlled components.</h4>
+                    <p>Answer: In React, controlled components refer to components that have their state and behavior controlled by the parent component. These components rely on props passed down from the parent component to update their state and behavior. Uncontrolled components refer to components that manage their own state internally.</p>
+                    <br />
+                    <h4>Question 2: How to validate React props using PropTypes?</h4>
+                    <p>Answer: PropTypes library. PropTypes allows you to define the type and shape of the props that are expected by a component, and will throw a warning in the console if any of the props do not meet the specified requirements.</p>
+                    <br />
+                    <h4>Question 3: Tell us the difference between nodejs and express js.</h4>
+                    <p>Answer: NodeJS is an event-driven, non-blocking I/O model using JavaScript as its main language. It helps to build scalable network applications. Express is a minimal and flexible Node. js web application framework that provides a robust set of features for web and mobile applications.</p>
+                    <br />
+                    <h4>Question 4: What is a custom hook, and why will you create a custom hook?</h4>
+                    <p>Answer: Custom React JS hooks offer reusability as when a custom hook is created, it can be reused easily, which makes the code cleaner and reduces the time to write the code. It also enhances the rendering speed of the code as a custom hook does not need to be rendered again and again while rendering the whole code.</p>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -19,14 +19,15 @@ const Footer = () => {
                     </div>
                     <br />
                     <div className=' mx-5 px-5'>
-                    <div>
-                        {/* <h6>Find and follow us on</h6> */}
-                        <FaTwitterSquare className='social-icons'/>
-                        <FaYoutube className='social-icons'/>
-                        <FaFacebook className='social-icons'/>
-                        <FaLinkedin className='social-icons'/>
-                    </div>
-                    
+                        <div>
+                            {/* <h6>Find and follow us on</h6> */}
+
+                            <a href=""><FaTwitterSquare className='social-icons' /></a>
+                            <a href=""><FaYoutube className='social-icons' /></a>
+                            <a href=""><FaFacebook className='social-icons' /></a>
+                            <a href=""><FaLinkedin className='social-icons' /></a>
+                        </div>
+
                         <br />
                         <p>Each recipe page includes a clear and concise list of ingredients, step-by-step instructions, and a photo of the finished dish. Users can also leave comments and ratings on each recipe, allowing others to see how successful they were in replicating the dish.</p>
                     </div>

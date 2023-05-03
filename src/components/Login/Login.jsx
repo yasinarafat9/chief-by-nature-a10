632fb6c97070980206f3bcff817f7fbf8cb37f02
yasinarafat9 <yasinarafat9612@gmail.com>
2023-05-03 11:31:34 +0600

    Email and password auth from firebase complete in login and registration page

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,22 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css'
 import { Link } from 'react-router-dom';
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import app from '../../firebase/firebase.config';
+
+
+const auth = getAuth(app);
 
 const Login = () => {
+
+    const [error, setError] = useState('');
+    const [success, SetSuccess] = useState('');
+    
+
+    const handleLogin = (event) => {
+        event.preventDefault();
+
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+        console.log(email, password)
+        
+        setError('');
+        SetSuccess('');
+        signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                // Signed in 
+                const user = userCredential.user;
+                console.log(user)
+                SetSuccess('Login Successfully')
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                setError(errorMessage);
+            });
+
+
+
+    }
+
+
+
+
+
+
+
     return (
         <div className='full-page'>
             <div className='login-card'>
-                <form >
+                <form onSubmit={handleLogin} >
                     <h2>Please Login</h2>
                     <br />
-
-
                     <input className='input-field' type="email" name='email' id='email' placeholder='Enter your email' />
 
                     <input className='input-field' type="password" name='password' id='password' placeholder=' Enter your password' />
                     <br />
-                    <button className='btn btn-primary my-3'>Log In</button>
-                    
+                    <p className='text-danger'>{error}</p>
+                    <p className='text-success'>{success}</p>
+                    <button type='submit' className='btn btn-primary my-3'>Log In</button>
+
                     <div>
                         <p className='mb-1 pb-0'>New to this Website?</p>
                         <Link className='btn btn-link mt-0 pt-0' to="/register"> Create an Account </Link>
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Register.css'
 import { Link } from 'react-router-dom';
+// import { initializeApp } from "firebase/app";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import app from '../../firebase/firebase.config';
+
+
+const auth = getAuth(app);
 
 const Register = () => {
+    // const [email, setEmail] = useState('');
+    const [error, setError] = useState('');
+    const [success, SetSuccess] = useState('');
+    
 
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const userName = event.target.userName.value;
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+        console.log(userName, email, password)
+
+        setError('');
+        SetSuccess('');
+        createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                // Signed in 
+                const user = userCredential.user;
+                console.log(user)
+                SetSuccess('Registration Successfully')
+            })
+            .catch((error) => {
+                // const errorCode = error.code;
+                const errorMessage = error.message;
+                setError(errorMessage);
+
+            });
+
+
+
+    }
 
 
 
@@ -11,22 +47,26 @@ const Register = () => {
     return (
         <div className='full-page'>
             <div className='register-card'>
-                <form >
+                <form onSubmit={handleSubmit}>
                     <h2>Please Register</h2>
                     <br />
-                    <input type="text" className='input-field' name='username' placeholder='Your Name' />
+                    <input type="text" className='input-field' name='userName' placeholder='Your Name' />
 
                     <input className='input-field' type="email" name='email' id='email' placeholder='Enter your email' />
 
                     <input className='input-field' type="password" name='password' id='password' placeholder=' Enter your password' />
-                    <button className='btn btn-primary my-3'>Register</button>
+                    <p className='text-danger'>{error}</p>
+                    <p className='text-success'>{success}</p>
+                    <button type='submit' className='btn btn-primary my-3'>Register</button>
+                    
 
                     <div>
-                    <p>Already have an Account? </p>
-                    <Link className='btn btn-link mt-0 pt-0' to="/login"> Log In</Link>
+                        <p className='mb-1 pb-0'>Already have an Account? </p>
+                        <Link className='btn btn-link mt-0 pt-0' to="/login"> Log In</Link>
 
                     </div>
                 </form>
+                
             </div>
         </div>
     );

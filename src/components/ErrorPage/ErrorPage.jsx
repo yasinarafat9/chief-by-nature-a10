74a20479ae5fba585fe088827399584479175008
yasinarafat9 <yasinarafat9612@gmail.com>
2023-05-04 15:10:48 +0600

    error page updated with image

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import './ErrorPage.css'
 import { Link } from 'react-router-dom';
+import errPng from '../../assets/images/error 404-min.png'
 
 const ErrorPage = () => {
     return (
         <div>
             <div className='err'>
-                <h1>404</h1>
-                <h2>Page not found</h2>
-                <Link to='/'><button className='btn-err-page-back'>Go back to home page</button></Link>
+                <img src={errPng} alt="" />
+                {/* <h1>404</h1>
+                <h2>Page not found</h2> */}
             </div>
+                <Link to='/'><button className='btn-err-page-back'>Go back to home page</button></Link>
         </div>
     );
 };
